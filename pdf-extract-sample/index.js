5f8cf5d7603e5d8939988424b2be9a758d496178
Gaurav Madaan <gaurav.madaan@techrbm.com>
2021-08-04 19:05:24 +0530

    Trying out options

--- pdf-extract-sample/index.js
+++ pdf-extract-sample/index.js
@@ -3,12 +3,20 @@ var filePath = path.join(__dirname, '1.pdf')
 var extract = require('pdf-text-extract')
 extract(filePath,
     {
-        splitPages: true
+        splitPages: true,
+        layout: 'raw'
     },
     function (err, pages) {
   if (err) {
     console.dir(err)
     return
   }
-  console.dir(pages[10])
+  let myResult = [];
+//   myResult = pages[0].split(/\r?\n/);
+//   myResult.forEach(myString => {
+//       console.log(myString);
+//       console.log("#sonia#");
+//   });
+  console.dir(pages[0]);
+
 })
